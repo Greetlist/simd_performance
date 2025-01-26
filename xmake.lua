cf52aacc7a4cd319ca969041f1dee0ebf5cc52ed
Greetlist <454281389@qq.com>
2025-01-26 10:43:18 +0800

    init repo

---
+++ xmake.lua
@@ -0,0 +1,7 @@
+add_rules("mode.debug", "mode.release")
+
+target("simd_performance")
+    set_kind("binary")
+    add_files("src/*.cc")
+    set_languages("c++17")
+--    add_cxflags("-stdnolib", "-fno-strict-aliasing")
