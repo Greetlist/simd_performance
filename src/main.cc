377a91a89d707a328271d364cd7ffc853180b03a
Greetlist <454281389@qq.com>
2025-02-21 16:51:13 +0800

    ignore and test case

--- src/main.cc
+++ src/main.cc
@@ -6,24 +6,24 @@
 constexpr int arr_size = 100000000;
 
 int main(int argc, char** argv) {
-  float* a = new(std::align_val_t{16}) float[arr_size];
-  float* b = new(std::align_val_t{16}) float[arr_size];
+  double* a = new(std::align_val_t{16}) double[arr_size];
+  double* b = new(std::align_val_t{16}) double[arr_size];
   for (int i = 0; i < arr_size; ++i) {
     a[i] = 10;
     b[i] = 20;
   }
-  Matrix<float> ma(30, 10000, 1.00);
-  Matrix<float> mb(10000, 10, 1.00);
+  Matrix<double> ma(1920, 1080, 2.00);
+  Matrix<double> mb(1080, 1920, 2.00);
 
-  BaseCalculator<float> bc;
+  BaseCalculator<double> bc;
   bc.MatrixMul(ma, mb);
   //bc.Mul(a, b, arr_size);
 
-  SSECalculator<float> ssec;
+  SSECalculator<double> ssec;
   ssec.MatrixMul(ma, mb);
   //ssec.Mul(a, b, arr_size);
 
-  AVXCalculator<float> avxc;
+  AVXCalculator<double> avxc;
   avxc.MatrixMul(ma, mb);
   //avxc.Mul(a, b, arr_size);
   return 0;
